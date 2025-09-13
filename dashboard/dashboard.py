c2ac63630c42f456eb000e9e23c2536359c83cf1
Luke Won <lukecywon@gmail.com>
2025-09-13 21:55:12 +0800

    Seperated get_video_id into helper.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,21 +1,10 @@
 import streamlit as st
 import pandas as pd
 import requests
-from urllib.parse import urlparse, parse_qs
 import os
+from helper import get_video_id
 from dotenv import load_dotenv 
 load_dotenv()
-
-
-# Extract video ID
-def get_video_id(youtube_url):
-    parsed_url = urlparse(youtube_url)
-    if parsed_url.hostname == "youtu.be":
-        return parsed_url.path[1:]
-    if parsed_url.hostname in ("www.youtube.com", "youtube.com"):
-        if parsed_url.path == "/watch":
-            return parse_qs(parsed_url.query)["v"][0]
-    return None
 
 st.set_page_config(page_title="NoogAI Analysis", layout="wide")
 st.title("Dashboard Prototype")
---
+++ dashboard/helper.py
@@ -0,0 +1,10 @@
+from urllib.parse import urlparse, parse_qs
+
+def get_video_id(youtube_url):
+    parsed_url = urlparse(youtube_url)
+    if parsed_url.hostname == "youtu.be":
+        return parsed_url.path[1:]
+    if parsed_url.hostname in ("www.youtube.com", "youtube.com"):
+        if parsed_url.path == "/watch":
+            return parse_qs(parsed_url.query)["v"][0]
+    return None
